f80a8b9717863e921e5c2de93b16ac465578fbff
Ian Vermes <ian.vermes@gmail.com>
2020-03-14 19:19:42 +0000

    Add ability to follow to next page and record if article is a revision

--- covid/scraper/scraper/spiders/medrxiv.py
+++ covid/scraper/scraper/spiders/medrxiv.py
@@ -1,8 +1,13 @@
 import scrapy
+import datetime
 
 TITLE_SELECTOR = "span.highwire-cite-title > a > span.highwire-cite-title::text"
 DOI_SELECTOR = "span.highwire-cite-metadata-doi::text"
 URL_SELECTOR = 'span.highwire-cite-title > a::attr("href")'
+DATE_FORMAT = "%B %d, %Y"
+
+DATE_SELECTOR = "div.pane-content h3.highwire-list-title::text"
+BOOKMARK = datetime.datetime(year=2020, month=3, day=6)
 
 
 class MedRXIVSpider(scrapy.Spider):
@@ -10,10 +15,37 @@ class MedRXIVSpider(scrapy.Spider):
     start_urls = ["https://www.medrxiv.org/content/early/recent?page=0"]
 
     def parse(self, response):
+        # Find all listed articles
         for section in response.css("div.pane-content > div.highwire-list-wrapper"):
             for article in section.css("div > ul > li"):
                 data = self._list_item_parser(article)
                 yield data
+
+        # Find the ling to the next page
+        next_page = response.css(
+            'div.highwire-list.page-group-last.item-list > ul > li > a::attr("href")'
+        ).get()
+
+        # Decide if following to next page
+        if self._follow_predicate(response) and next_page is not None:
+            self.logger.info(f"Follow to next page: {next_page}")
+            yield response.follow(next_page, callback=self.parse)
+        else:
+            self.logger.info(
+                f"Do not follow to next page, bookmark reached: {BOOKMARK}"
+            )
+
+    def _follow_predicate(self, response):
+        date_string = response.css(DATE_SELECTOR).get()
+        if not date_string:
+            self.logger.error("No title date found on page!")
+            return false
+        date = datetime.datetime.strptime(date_string, DATE_FORMAT)
+        # return date > BOOKMARK
+        return False
+
+    def _is_article_revision(self, url):
+        return url is not None and url.endswith("v1")
 
     def _list_item_parser(self, item):
         # Extract data
@@ -27,4 +59,10 @@ class MedRXIVSpider(scrapy.Spider):
             value = data[key]
             if value:
                 data[key] = value.strip()
+        # Concat URL with domain
+        url = data.get("url")
+        if url:
+            data["url"] = "https://www.medrxiv.org" + url
+        # Add `is_revision` key and boolean value
+        data["is_revision"] = self._is_article_revision(data.get("url"))
         return data
